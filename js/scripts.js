581e1bf18aa816c19d02560f9935d478fc53e271
santa claus <haji.abdiaziz10@gmail.com>
2019-09-30 09:41:36 +0300

    add js

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,22 @@ $(document).ready(function(){
     $("#product").toggle();
   });
 });
+$(document).ready(function(){
+  $("form.form").submit(function(event){
+    var name = $("input#name").val();
+    var email = $("input#email").val();
+    var message = $ ("input#message").val();
+    if ($("input#name") && $("input#email").val());
+      else if (true) {
+        (name + ", We have received your message.Thank you for reaching out.");
+      }
+      else if (false) {
+        ("please type your information")
+      }
+
+    });
+
+
+
+
+});
